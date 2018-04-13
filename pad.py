89e60560186d58d90b740378e08406bb8d3fb3d3
huan <1214739508@qq.com>
2018-04-13 15:44:10 +0800

    第三次提交pad

--- pad.py
+++ pad.py
@@ -1,9 +1,9 @@
 
-import tensorflow as tf
-with tf.Session() as sess:  
-    inputs = [[1],[2],[3]]
-    padding = [[1,2],[1,2]]
-    print(sess.run(tf.pad(inputs,padding)))
+# import tensorflow as tf
+# with tf.Session() as sess:  
+#     inputs = [[1],[2],[3]]
+#     padding = [[1,2],[1,2]]
+#     print(sess.run(tf.pad(inputs,padding)))
 
 
 
@@ -11,3 +11,5 @@ with tf.Session() as sess:
 # Animal = namedtuple('Animal2','name age type')
 # big_yellow = Animal(name="big_yellow",age = 3,type = "dog")
 # print(big_yellow.age)
+
+print
