80fa25fad7a093b76902eccefcaabba294eaa0ec
huan <1214739508@qq.com>
2018-04-29 15:50:22 +0800

    Pickle将一个二维数组写入文件，再读取文件，pickle可以将任意一个对象比如mat即矩阵，写入文件

---
+++ pickledemo.py
@@ -0,0 +1,14 @@
+
+###############将二维数组整体写入文件，读取文件中的内容#############
+import  pickle
+
+a = [[1,2],[2,3]]
+#写入文件
+f1 = open("/Users/huan/code/pythondemo/test.txt","wb")
+pickle.dump(a,f1)
+f1.close()
+#从文件中读取数据
+f2 = open("/Users/huan/code/pythondemo/test.txt",'rb')
+test_f = pickle.load(f2)
+print(test_f)
+f2.close()
