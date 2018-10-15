7f1703d6fcafc3b3f14405e37ca5372e0c48fe2d
huan <huan@huan.local>
2018-10-15 17:55:17 +0800

    pyqt多线程

---
+++ pyqtthread/pyqtthread/getPostsThread.py
@@ -0,0 +1,21 @@
+# -*- coding:UTF-8 -*-
+
+from PyQt5.QtCore import  QThread,pyqtSignal
+import time
+class getPostsThread(QThread):
+    #声明一个信号
+    show_text = pyqtSignal(str)
+    def __init__(self):
+        QThread.__init__(self)
+
+
+    def __del__(self):
+        self.wait()
+
+    def run(self):
+        time.sleep(2)
+        #自线程发出信号
+        self.show_text.emit("我是子线程的text")
+        time.sleep(2)
+        print("this is run")
+
